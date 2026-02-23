25060ad1f7ddaf3deff06b616097161001dad45a
Maximilian Walterskirchen <me@mwalterskirchen.dev>
2026-02-23 14:03:21 +0100

    Initial Commit

---
+++ znueni/znueniApp.swift
@@ -0,0 +1,17 @@
+//
+//  znueniApp.swift
+//  znueni
+//
+//  Created by Maximilian Walterskirchen on 23.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct znueniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
